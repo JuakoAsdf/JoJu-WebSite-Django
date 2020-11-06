f92c6f1a08bc5714a22a50b6f33aff74cd14e9de
Junisses <brandon.sandovalsan@gmail.com>
2020-11-06 20:07:23 -0300

    CRUD y ORM V1

--- PetShop/gestorProducto/models.py
+++ PetShop/gestorProducto/models.py
@@ -15,21 +15,31 @@ class Categoria(models.Model):
         return self.nombre
 
 class Sucursal(models.Model):
-    nombre        = models.TextField(max_length=50)
+    nombre       = models.TextField(max_length=50)
     direccion    = models.TextField(max_length=100)
-    telefono    = models.TextField(max_length=9)
+    telefono     = models.TextField(max_length=9)
     encargado    = models.TextField(max_length=60)
 
+    def str(self):
+        return self.nombre
 
 class Producto(models.Model):
     marca        = models.ForeignKey(Marca, blank=True, null=True, on_delete=models.SET_NULL)
     categoria    = models.ForeignKey(Categoria, blank=True, null=True, on_delete=models.SET_NULL)
     nombre       = models.TextField(max_length=60)
-    codigo        = models.DecimalField(max_digits=13, decimal_places=0)
-    descripcion = models.TextField(max_length = 200)
+    codigo       = models.DecimalField(max_digits=13, decimal_places=0)
+    descripcion  = models.TextField(max_length = 200)
     stock        = models.IntegerField()
-    precioCosto = models.IntegerField()
-    precioVenta = models.IntegerField()
+    precioCosto  = models.IntegerField()
+    precioVenta  = models.IntegerField()
 
     def str(self):
         return self.descripcion
+
+class Adopcion(models.Model):
+    nombre       = models.TextField(max_length=60)
+    descripcion  = models.TextField(max_length = 200)
+    activo        = models.BooleanField()
+
+    def str(self):
+        return self.nombre
--- PetShop/gestorProducto/views.py
+++ PetShop/gestorProducto/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from gestorProducto.models import Adopcion
 # Create your views here.
 
 def inicio(request):
@@ -23,7 +24,51 @@ def accesoriodog(request):
     return render(request,'accesoriodog.html', {})
     
 def adopcion(request):
-    return render(request,'adopcion.html', {})
+    mensaje = "";
+    lista = {}
+    item = {}
+    
+    if request.method == "POST":
+        id            = int("0" + request.POST["txtId"])
+        nombre        = request.POST["txtNombre"]
+        descripcion   = request.POST["txtDescripcion"]
+        activo        = request.POST.get("chkActivo") is "1" 
+        
+        if 'btnGrabar' in request.POST:
+        
+            if id < 1: 
+                Adopcion.objects.create(nombre = nombre, descripcion = descripcion, activo = activo) #registra los datos
+            else:
+                item = Adopcion.objects.get(pk = id)
+                item.nombre = nombre
+                item.descripcion = descripcion
+                item.activo = activo
+                item.save() #guarda los cambios
+                item = {}
+            
+            mensaje = "Datos guardados"
+
+        elif 'btnBuscar' in request.POST:
+            try:
+                item = Adopcion.objects.get(pk = id) 
+            except:
+                mensaje = "Registro no encontrado"
+                item = {}
+                
+             
+        elif 'btnListar' in request.POST:
+            lista = Adopcion.objects.all()
+        
+        elif 'btnEliminar' in request.POST:
+            item = Adopcion.objects.get(pk = id) #obtiene el registro segun id
+            
+            if isinstance(item, Adopcion):
+                item.delete()
+                mensaje = "Registro eliminado"
+                item = {}
+                
+    contexto = {'mensaje' : mensaje, 'lista' : lista, 'item' : item}
+    return render(request,'adopcion.html', contexto)
     
 def nosotros(request):
     return render(request,'nosotros.html', {})
